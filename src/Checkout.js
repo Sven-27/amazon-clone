0429819c17ad4450b3089650c968bcddacf57749
Sven-27 <sven.notermans@live.nl>
2020-09-22 19:39:32 +0200

    just build the shoppingcart

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import "./Checkout.css";
-import Subtotal from './Subtotal';
+import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
  
     return (
         <div className="checkout">
@@ -14,10 +18,18 @@ function Checkout() {
 
                 <div>
                     <h2 className="checkout__title">Your shopping Basket</h2>
-                    {/*CheckoutProduct */}
-                    {/*CheckoutProduct */}
-                    {/*CheckoutProduct */}
-                    {/*CheckoutProduct */}
+                    
+                {basket.map(item =>( 
+                        <CheckoutProduct 
+                         id={item.id}
+                        title={item.title}
+                        
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                       
+                    ))}
                 </div>
             </div>
 
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './CheckoutProduct.css';
 
-function CheckoutProduct(id, image, title, price, rating) {
+function CheckoutProduct({id, image, title, price, rating}) {
+
+    const removeFromBasket = () => {
+        //remove the item from the basket
+        dispatchEvent({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+            
+        })
+    }
+    
     return (
         <div className="checkoutProduct">
            <img className="checkoutProduct__image" src={image} /> 
@@ -14,11 +24,11 @@ function CheckoutProduct(id, image, title, price, rating) {
                      <div className="checkoutProduct__rating">
                          {Array(rating)
                          .fill()
-                         .map((_, i) => (
+                         .map(() => (
                              <p>🤘</p>
                          ))}
                      </div>
-                     <button>Remove from Basket</button>
+                     <button onClick={removeFromBasket}>Remove from Basket</button>
            </div>
         </div>
     )
